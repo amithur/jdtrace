f8029a380b987dac8f703c5c2cfee0c017e4f6f7
amithurvitz <amithurvitz@de5df254-687d-6a90-fc46-bfa66320a310>
2016-01-11 17:56:46 +0000

    Creating release branch 1.1.0

---
+++ dev/src/java/src/net/java/jdtrace/client/DescriptionConverter.java
@@ -0,0 +1,96 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.java.jdtrace.client;
+
+import java.util.StringTokenizer;
+
+/**
+ *
+ * @author amit
+ */
+class DescriptionConverter {
+    String origProbeDescription;
+    String pid, pidSubs;
+    String classPattern, methodPattern;
+    String probeName;
+    ArgParser argParser;
+    boolean endsWithComma = false;
+
+    public DescriptionConverter(String line, ArgParser ap) {
+        origProbeDescription = line;
+        argParser = ap;
+        parseProbeDescription();
+    }
+
+    String convert(String line) {
+        String convertedName;
+        if (probeName.equals("entry")) {
+            convertedName = "method_entry";
+        }
+        else if (probeName.equals("return")) {
+            convertedName = "method_return";
+        }
+        else {
+            convertedName = ".*";
+        }
+        return "VvmJsdtProvider" + pidSubs + ":::" + convertedName;
+    }
+    
+    public String getPid() {
+        return pid;
+    }
+    
+    public String getClassPattern() {
+        return classPattern;
+    }
+    
+    public String getMethodPattern() {
+        return methodPattern;
+    }
+
+    public String getProbeName() {
+        return probeName;
+    }
+    private void parseProbeDescription() {
+        String [] tokens;
+        tokens = origProbeDescription.split(":", 4);
+        if (tokens.length < 4) return; // not good... should throw an exception
+        if (tokens[0].matches("jdtrace.*")) {
+            pidSubs = tokens[0].substring(7);
+            if (pidSubs.matches("\\$\\d+")) {
+                pid = argParser.getFreeArg(new Integer(pidSubs.substring(1)).intValue());
+            }
+            else if (pidSubs.matches("\\d+")) {
+                pid = pidSubs;
+                pid.trim();
+            }
+            else if (pidSubs.equals("$target")) 
+                pid = argParser.getTarget();
+        } else {
+            return;
+        }
+        if (tokens[1].isEmpty())
+            classPattern = ".*";
+        else
+            classPattern = tokens[1];
+        if (tokens[2].isEmpty())
+            methodPattern = ".*";
+        else
+            methodPattern = tokens[2];
+        if (origProbeDescription.matches(".*,\\s*$")) {
+            endsWithComma = true;
+            tokens[3] = tokens[3].substring(0, tokens[3].indexOf(","));
+        }
+        if (tokens[3].isEmpty())
+            probeName = ".*";
+        else
+            probeName = tokens[3];
+    }
+    
+    public boolean commaAttheEnd() {
+        return endsWithComma;
+    }
+    
+}
---
+++ dev/src/java/src/net/java/jdtrace/client/ScriptRunner.java
@@ -0,0 +1,82 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.java.jdtrace.client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author amit
+ */
+class ScriptRunner {
+
+    private String fileNameToWaitFor;
+    private boolean waitForJdtraceProvider;
+    private Process dtraceProcess = null;
+
+    public ScriptRunner(boolean waitJdtraceProvider) {
+        fileNameToWaitFor = null;
+        waitForJdtraceProvider = waitJdtraceProvider;
+    }
+
+    private void attachShutDownHook() {
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                System.out.println("Inside Add Shutdown Hook : " + Thread.currentThread().getName());
+                if (dtraceProcess != null) {
+                    System.out.println("want to kill process " + dtraceProcess.toString());
+                }
+            }
+        });
+    }
+
+    void runScript(String[] args
+    ) {
+        boolean success;
+        try {
+            String cmd[] = new String[args.length + 1];
+            if (waitForJdtraceProvider) {
+                Runtime.getRuntime().exec(Utils.getJdtraceHome() + "/check_for_dtrace_provider.sh");
+                success = Utils.waitUntilFileCreated(fileNameToWaitFor, 8000);
+                if (!success) {
+                    // should throw an exception!
+                    System.err.println("DTrace provider creation timeout");
+                    System.exit(1);
+                }
+            }
+            cmd[0] = "/usr/sbin/dtrace";
+            System.arraycopy(args, 0, cmd, 1, args.length);
+            dtraceProcess = Runtime.getRuntime().exec(cmd);
+            String line;
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(dtraceProcess.getInputStream()));
+            BufferedReader err = new BufferedReader(
+                    new InputStreamReader(dtraceProcess.getErrorStream()));
+            while ((line = in.readLine()) != null) {
+                System.out.println(line);
+            }
+            while ((line = err.readLine()) != null) {
+                System.err.println(line);
+            }
+            in.close();
+            err.close();
+            dtraceProcess.waitFor();
+        } catch (IOException ex) {
+            Logger.getLogger(ScriptRunner.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(ScriptRunner.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void setFileToWaitFor(String filePath) {
+        fileNameToWaitFor = filePath;
+    }
+
+}
